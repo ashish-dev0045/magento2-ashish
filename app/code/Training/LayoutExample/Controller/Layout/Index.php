25369b06c2b67b696095b0ffbad10b6f599d7758
Ashish gupta <gupta.ashish0045@gmail.com>
2024-08-14 10:50:15 +0530

    video-7

---
+++ app/code/Training/LayoutExample/Controller/Layout/Index.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Training\LayoutExample\Controller\Layout;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index implements ActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    protected $pageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * Execute action based on request and return result
+     *
+     * @return ResultInterface
+     */
+    public function execute()
+    {
+        $result = $this->pageFactory->create();
+        $result->getConfig()->getTitle()->set('Custom title overwritten');
+        // $result->getLayout()->getBlock('page.main.title')->setPageTitle('Software Developer');
+        return $result;
+    }
+}
